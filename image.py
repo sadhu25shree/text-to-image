bdfb90ada1d1f8cab1e96090a4827202bef11f14
sadhu25shree <159885096+sadhu25shree@users.noreply.github.com>
2024-08-22 22:14:29 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,19 @@
+import requests
+import streamlit as st
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/sdxl-turbo"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs":st.text_input('Enter Prompt'),
+})
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
+
+if st.button('Generate'):
+    st.image(image)
